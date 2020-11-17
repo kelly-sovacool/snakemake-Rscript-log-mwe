b4e2289ff46fd8f9dcc66a742c6cf57586a59ae3
Kelly Sovacool <kelly-sovacool@users.noreply.github.com>
2020-11-16 21:49:04 -0500

    Update script.R

--- script.R
+++ script.R
@@ -1,8 +1,11 @@
-if (length(snakemake@log) != 0) {
-  log <- file(snakemake@log[1][[1]], open = "wt")
-  sink(log, append = TRUE)
-  sink(log, append = TRUE, type = "message")
+log_smk <- function() {
+  if (exists("snakemake") & length(snakemake@log) != 0) {
+    log <- file(snakemake@log[1][[1]], open = "wt")
+    sink(log, append = TRUE)
+    sink(log, append = TRUE, type = "message")
+  }
 }
 
+log_smk()
 message('Hello World')
 file.create(snakemake@output[['txt']])
